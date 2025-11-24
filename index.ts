ad49f4ce6c762bc73c848302e6e3050af69f00b8
36000.eth <1982775886@qq.com>
2025-11-24 20:31:37 +0800

    feat: base code

---
+++ index.ts
@@ -0,0 +1,18 @@
+import { generateText } from 'ai';
+import { createOpenRouter } from '@openrouter/ai-sdk-provider';
+
+const openrouter = createOpenRouter({
+  apiKey: process.env.OPENROUTER_API_KEY,
+});
+
+const model = openrouter('openai/gpt-4o');
+
+async function main() {
+  const { text } = await generateText({
+    model,
+    prompt: 'Write a vegetarian lasagna recipe for 4 people.',
+  });
+  console.log(text);
+}
+
+main();
